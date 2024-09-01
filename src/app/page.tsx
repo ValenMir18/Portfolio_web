d85ff90339547abcafd614add383cb05671e50c1
ValenMir18 <mpvalu17@gmail.com>
2024-08-31 22:20:32 -0500

    Actualización cv y cambio de portada

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,10 @@ config.autoAddCss = false;
 
 export default function Home() {
   return (
-    <section id="home" className="flex flex-col xs:mb-24 xs:mt-16 lg:flex-row lg:my-20 md:mx-16 sm:mx-16">
+    <section
+      id="home"
+      className="flex flex-col-reverse items-center columns-2 xs:mb-24 xs:mt-16 lg:flex-row lg:my-12 md:mx-16 sm:mx-16"
+    >
       <div className="text-center md:px-9 md:ml-10 lg:text-left lg:w-[900px]">
         <h1 className="font-bold lg:text-6xl sm:text-5xl text-6xl tracking-tight text-slate-200">
           VALENTINA MIRANDA
@@ -22,71 +25,81 @@ export default function Home() {
           Web developer
         </h2>
         <p className="my-4 text-lg lg:w-[550px] lg:text-justify text-center text-slate-500 tracking-tight md:text-2xl ">
-          Hi, welcome to my digital corner, where lines of code tell stories. 
-          I am a software developer dedicated to creating innovative solutions that transform ideas into reality.
+          Hi, welcome to my digital corner, where lines of code tell stories. I
+          am a software developer dedicated to creating innovative solutions
+          that transform ideas into reality.
         </p>
         <div className="flex lg:hidden justify-center my-8 pb-4 ">
-        <ul className="flex gap-4">
-        <li>
-          <a
-            href="https://github.com/ValenMir18"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="hover:text-rose-600 text-white"
-          >
-            <FontAwesomeIcon
-              icon={faGithub}
-              style={{ fontSize: "1.8rem" }}
-              title="Github"
-            />
-          </a>
-        </li>
-        <li>
-          <a
-            href="https://www.instagram.com/prasca__v?igsh=MWsybWZqZzYxYXMxeQ=="
-            target="_blank"
-            rel="noopener noreferrer"
-            className="hover:text-rose-600 text-white"
-          >
-            <FontAwesomeIcon
-              icon={faInstagram}
-              style={{ fontSize: "1.8rem" }}
-              title="Instagram"
-            />
-          </a>
-        </li>
-        <li>
-          <a
-            href="https://twitter.com/valMpr"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="hover:text-rose-600 text-white"
-          >
-            <FontAwesomeIcon
-              icon={faXTwitter}
-              style={{ fontSize: "1.8rem" }}
-              title="X"
-            />
-          </a>
-        </li>
-        <li>
-          <a
-            href="https://www.linkedin.com/in/valenmiranda/"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="hover:text-rose-600 text-white"
-          >
-            <FontAwesomeIcon
-              icon={faLinkedin}
-              style={{ fontSize: "1.8rem" }}
-              title="Linkedin"
-            />
-          </a>
-        </li>
-      </ul>
+          <ul className="flex gap-4">
+            <li>
+              <a
+                href="https://github.com/ValenMir18"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="hover:text-rose-600 text-white"
+              >
+                <FontAwesomeIcon
+                  icon={faGithub}
+                  style={{ fontSize: "1.8rem" }}
+                  title="Github"
+                />
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.instagram.com/prasca__v?igsh=MWsybWZqZzYxYXMxeQ=="
+                target="_blank"
+                rel="noopener noreferrer"
+                className="hover:text-rose-600 text-white"
+              >
+                <FontAwesomeIcon
+                  icon={faInstagram}
+                  style={{ fontSize: "1.8rem" }}
+                  title="Instagram"
+                />
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://twitter.com/valMpr"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="hover:text-rose-600 text-white"
+              >
+                <FontAwesomeIcon
+                  icon={faXTwitter}
+                  style={{ fontSize: "1.8rem" }}
+                  title="X"
+                />
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.linkedin.com/in/valenmiranda/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="hover:text-rose-600 text-white"
+              >
+                <FontAwesomeIcon
+                  icon={faLinkedin}
+                  style={{ fontSize: "1.8rem" }}
+                  title="Linkedin"
+                />
+              </a>
+            </li>
+          </ul>
         </div>
         <ContactMeLink />
-        
+      </div>
+      <div className="flex justify-center items-center lg:justify-start lg:pt-16 mb-12 mt-2 lg:mt-0">
+        <div className="relative w-[250px] h-[250px] animate-bounce-slow">
+          <img
+            src="/Imagenes/avatar1.jpg"
+            alt="Avatar"
+            className="w-full h-full object-cover rounded-full"
+          />
+          <div className="absolute top-0 left-0 w-full h-full border-2 border-white box-border rounded-full"></div>
+        </div>
       </div>
     </section>
   );
--- src/pages/about/page.tsx
+++ src/pages/about/page.tsx
@@ -1,8 +1,7 @@
-
 export default function About() {
   return (
     <section
-      className="flex flex-col-reverse lg:flex-row lg:mx-24 lg:mt-24 md:mt-12 md:px-12 "
+      className="flex  lg:flex-row lg:mx-24 lg:mt-24 md:mt-12 md:px-12 "
       id="about"
     >
       <div className="flex flex-col md:mx-16 sm:mx-10 xs:mx-10 ">
@@ -19,7 +18,7 @@ export default function About() {
             memorable web experiences for users.
           </p>
           <p className="text-justify tracking-tight lg:text-[16px] text-[20px] mt-3 text-white">
-            I worked closely with the product team at accounting firm {" "}
+            I worked closely with the product team at accounting firm{" "}
             <a
               href="https://www.siigo.com/nosotros/"
               target="_blank"
@@ -27,33 +26,21 @@ export default function About() {
               className="text-blue-500 hover:underline"
             >
               Siigo
-            </a>,
-            where I contributed to the development of innovative tools that
-            simplify and improve our users&apos; experience on the firm&apos;s website.
-            But my passion for programming goes beyond work hours; Independently
-            and as part of my hobby, I immerse myself in creative projects where
-            I can explore new ideas and technologies.
+            </a>
+            , where I contributed to the development of innovative tools that
+            simplify and improve our users&apos; experience on the firm&apos;s
+            website. But my passion for programming goes beyond work hours;
+            Independently and as part of my hobby, I immerse myself in creative
+            projects where I can explore new ideas and technologies.
           </p>
           <p className="text-justify tracking-tight lg:text-[16px] text-[20px] mt-3 text-white">
-            I am committed to my professional and academic growth. I am
-            currently studying systems engineering at the University of Córdoba,
-            where I am strengthening my skills in the world of code, a journey
-            that I began in 2023 and that continues to fill me with excitement
-            and constant learning.
+            I am committed to my professional growth and continue to strengthen
+            my skills in the world of code, a journey that fills me with
+            excitement and constant learning.
           </p>
         </div>
       </div>
-      <div className="flex justify-center lg:justify-start lg:pt-16 mb-12 mt-2 lg:mt-0">
-        <div className="relative w-[250px] h-64">
-          <img
-            src="/Imagenes/avatar1.jpg"
-            alt="Avatar"
-            className="w-full h-full object-cover"
-          />
-          <div className="absolute top-0 left-0 w-full h-full border-2 border-white box-border"></div>
-          <div className="absolute top-0 left-0 w-full h-full border-2 border-white box-border transform translate-x-4 translate-y-4 -z-10"></div>
-        </div>
-      </div>
+      
     </section>
   );
 }
